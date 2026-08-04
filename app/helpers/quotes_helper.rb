8eb1d28eb94fe5c8b0ec3dbe9b3a8baa4f4c69e0
acouprie <antoine@couprie.dev>
2026-08-04 21:40:48 +0200

    :lipstick: implement quote list UI

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,5 +1,13 @@
 module QuotesHelper
+  PLUS_ICON = <<~SVG.html_safe
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+      <path d="M12 5v14M5 12h14" />
+    </svg>
+  SVG
+
   def new_quote_trigger_link
-    link_to t("quotes.new_quote"), new_quote_path, data: { turbo_frame: "new_quote" }
+    link_to new_quote_path, class: "button", data: { turbo_frame: "new_quote" } do
+      safe_join([ PLUS_ICON, t("quotes.new_quote") ])
+    end
   end
 end
